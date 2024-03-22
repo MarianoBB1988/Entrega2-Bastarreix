543735a7f7e3192b35b543c95cfc0ce414d5b968
Mariano Bastarreix Blanco <mariano.bastarreix@gmail.com>
2024-03-21 21:45:31 -0300

    update

--- src/components/product/SingleProduct.jsx
+++ src/components/product/SingleProduct.jsx
@@ -2,8 +2,10 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { FaRegStar, FaStar, FaStarHalfAlt } from "react-icons/fa";
-
-export default function SingleProduct({agregar, cont}) {
+import Fade from 'react-bootstrap/Fade';
+import FsLightbox from "fslightbox-react";
+export default function SingleProduct({ agregar, cont }) {
+    const [open, setOpen] = useState(false);
 
     const [contador, setContador] = useState(cont);
     const { prodId } = useParams();
@@ -12,13 +14,23 @@ export default function SingleProduct({agregar, cont}) {
 
     const [product, setProduct] = useState([]);
     console.log('id: ' + prodId + catId);
+    const [lightboxController, setLightboxController] = useState({
+        toggler: false,
+
+    });
 
     const incrementar = () => {
         setContador(cont + 1);
-    
+
         agregar(contador)
-      };
-    
+    };
+
+    function abrirLigthbox(ruta) {
+        setLightboxController({
+            toggler: !lightboxController.toggler,
+            ruta: ruta
+        });
+    }
 
     const imgEstilo = {
         width: '300px',
@@ -33,6 +45,18 @@ export default function SingleProduct({agregar, cont}) {
         fontSize: '12px',
     };
 
+
+    const mostrar = new Promise(() => {
+
+        setTimeout(() => {
+
+
+            setOpen(true)
+            // reject('Error en servidor');
+
+        }, 1000);
+
+    });
 
 
 
@@ -82,6 +106,7 @@ export default function SingleProduct({agregar, cont}) {
 
 
 
+
     return (
         <>
             <br></br>
@@ -89,54 +114,66 @@ export default function SingleProduct({agregar, cont}) {
             <br></br>
             <br></br>
             <br></br>
-            
 
-            <div className="row " >
-                <div className="col-8 "> <a className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0" href="#"> <Link style={linkStyle} to={'/products/' + catId}>Volver &nbsp; | &nbsp;  </Link> </a>
-                    <a className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0" href="#" style={linkStyle}> <Link style={linkStyle} to={'/'}>Home</Link> /</a>
-                    <a className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0" href="#"> <Link style={linkStyle} to={'/products/' + catId}>{ruta(catId)}</Link></a><br></br>
+
+            <FsLightbox
+                toggler={lightboxController.toggler}
+
+                sources={[lightboxController.ruta]}
+            />
+
+            <Fade in={open}>
+
+                <div className="row " >
+                    <div className="col-8 "> <a className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0" href="#"> <Link style={linkStyle} to={'/products/' + catId}>Volver &nbsp; | &nbsp;  </Link> </a>
+                        <a className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0" href="#" style={linkStyle}> <Link style={linkStyle} to={'/'}>Home</Link> /</a>
+                        <a className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0" href="#"> <Link style={linkStyle} to={'/products/' + catId}>{ruta(catId)}</Link></a><br></br>
+                    </div>
+
                 </div>
-
-            </div>
+            </Fade>
 
             {products.filter(producto => producto.id == prodId).map((producto) => (
+                <Fade in={open}>
 
-                <div className="card shadow" >
+                    <div className="card shadow" >
 
-                    <p className="card-header">Detalles del producto</p>
-                    <div className="row" >
-                    <div className="card-body col-1">
+                        <p className="card-header">Detalles del producto</p>
+                        <div className="row" >
+                            <div className="card-body col-1">
                             </div>
-                        <div className="card-body col-4"  >
-                            <img class="img-fluid" alt="Responsive image" style={imgEstilo} src={'http://http2.mlstatic.com/D_' + producto.thumbnail_id + '-O.webb'} />
+                            <div className="card-body col-4"  >
+                                <a href="#" onClick={() => abrirLigthbox('http://http2.mlstatic.com/D_' + producto.thumbnail_id + '-O.webb')}><img class="img-fluid" alt="Responsive image" style={imgEstilo} src={'http://http2.mlstatic.com/D_' + producto.thumbnail_id + '-O.webb'} /></a>
+
+                            </div>
+                            <div className="card-body col-4">
+
+                                <p className="card-text">{producto.title}</p>
+                                <h4 className="card-title">{producto.currency_id} <strong>{producto.price}</strong></h4>
+                                <h6>Valoración: </h6>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStarHalfAlt /></i>
+                                <i><FaRegStar /></i>
+                                <br></br>
+                                <br></br>
+                                <h6 className="card-title">Disponibles: <strong>{producto.available_quantity}</strong></h6>
+                                <h6 className="card-title">Condición: <strong>{suplantar(producto.condition)}</strong></h6><br></br>
+
+                                <div className="d-grid gap-2">
+                                    <button className="btn btn-secondary" type="button" data-mdb-ripple-init onClick={incrementar}>Agregar al carrito</button>
+                                    <button className="btn btn-dark " type="button" data-mdb-ripple-init>Comporar ahora</button>
+                                </div>
+                            </div>
+
+                            <div className="card-body col-1">
+                            </div>
+
                         </div>
-                        <div className="card-body col-4">
-
-                            <p className="card-text">{producto.title}</p>
-                            <h4 className="card-title">{producto.currency_id} <strong>{producto.price}</strong></h4>
-                            <h6>Valoración: </h6>
-                            <i><FaStar /></i>
-                            <i><FaStar /></i>
-                            <i><FaStar /></i>
-                            <i><FaStarHalfAlt /></i>
-                            <i><FaRegStar /></i>
-                            <br></br>
-                            <br></br>
-                            <h6 className="card-title">Disponibles: <strong>{producto.available_quantity}</strong></h6>
-                            <h6 className="card-title">Condición: <strong>{suplantar(producto.condition)}</strong></h6><br></br>
-
-                            <div className="d-grid gap-2">
-                                <button className="btn btn-secondary" type="button" data-mdb-ripple-init onClick={incrementar}>Agregar al carrito</button>
-                                <button className="btn btn-dark" type="button" data-mdb-ripple-init>Comporar ahora</button>
-                            </div>
-                        </div>
-
-                        <div className="card-body col-1">
-                            </div>
-
                     </div>
-                </div>
 
+                </Fade>
 
             ))}
 
--- src/components/products/ProductsComponent.jsx
+++ src/components/products/ProductsComponent.jsx
@@ -1,14 +1,20 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import NavBarComponent from '../NavBar/NavBarComponent';
+import Fade from 'react-bootstrap/Fade';
+import { FaRegStar, FaStar, FaStarHalfAlt } from "react-icons/fa";
+
 
 export default function ProductsComponent({ agregar, cont }) {
-
+  const [open, setOpen] = useState(true);
   const [contador, setContador] = useState(1);
   const { catId } = useParams();
   const [products, setProducts] = useState([]);
+  
   console.log('id: ' + catId);
+ 
+
+
 
   //incrementamos el carrito
   const incrementar = () => {
@@ -23,7 +29,8 @@ export default function ProductsComponent({ agregar, cont }) {
 
   }
   const imgEstilo = {
-    width: '40%'
+    width: '40%',
+  
   }
 
   const btnEstilo = {
@@ -31,19 +38,30 @@ export default function ProductsComponent({ agregar, cont }) {
   }
 
 
+new Promise(() => {
+
+    setTimeout(() => {
+
+      setOpen(true)
+
+    },1000);
+  });
 
 
   useEffect(() => {
-
+   
     fetch('https://api.mercadolibre.com/sites/MLU/search?category=' + catId)
       .then(response => response.json())
+      .then(setOpen(false))
+      
       .then(data => {
         console.log(data.results)
+        
         setProducts(data.results);
         setContador(cont + 1);
-
+       
       });
-
+      
   }, catId);
 
   return (
@@ -57,33 +75,36 @@ export default function ProductsComponent({ agregar, cont }) {
       <br></br>
 
 
+      <Fade in={open}>
+        <div className="row row-cols-3 g-3">
 
-      <div className="row row-cols-3 g-3">
+          {products.map((producto) => (
 
-        {products.map((producto) => (
-          <div className="col">
-            <div className="card shadow">
-              <img style={imgEstilo} src={producto.thumbnail} className="card-img-top"
-                alt="Hollywood Sign on The Hill" />
-              <div className="card-body">
-
-                <p className="card-text">
-                  {producto.title}
-                </p>
-                <h5 className="card-title">{producto.currency_id} <strong>{producto.price}</strong></h5>
+            <div className="col">
+              <div className="card shadow">
+              <a href='#' className="btn btn-light btn-link" data-mdb-ripple-init data-mdb-ripple-color="dark"><Link to={'/product/' + producto.id + '/' + catId}><img style={imgEstilo} src={producto.thumbnail} className="card-img-top"
+                  alt="imagen" /></Link></a>
                
-                <button type="button" className="btn btn-dark" data-mdb-ripple-init onClick={()=>incrementar()} >Agregar al carrito</button>
-                <a href='#' className="btn btn-light btn-link" data-mdb-ripple-init data-mdb-ripple-color="dark"><Link to={'/product/' + producto.id + '/' + catId}>Ver detalles</Link></a>
-            
+                <div className="card-body">
 
+                  <p className="card-text">
+                    {producto.title}
+                  </p>
+                  <h5 className="card-title">{producto.currency_id} <strong>{producto.price}</strong></h5>
+
+                  <button type="button" className="btn btn-dark" data-mdb-ripple-init onClick={() => incrementar()} >Agregar al carrito</button>
+                  <a href='#' className="btn btn-light btn-link" data-mdb-ripple-init data-mdb-ripple-color="dark"><Link to={'/product/' + producto.id + '/' + catId}>Ver detalles</Link></a>
+
+
+                </div>
               </div>
+
             </div>
-          </div>
-        ))}
-      </div>
 
+          ))}
 
-
+        </div>
+      </Fade>
     </>
   );
 
